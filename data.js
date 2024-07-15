502afb76dfa7b380632a87ea8aedc7913b3628d4
thapelobaps <75887730+thapelobaps@users.noreply.github.com>
2024-07-15 12:57:29 +0200

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const data = [
     {
         itemName: "Nanotech Fabrics",
         itemCategory: "Eco-Futurism Line",
-        itemLink: "https://www.youtube.com/@codegrid",
+        itemLink: "https://www.youtube.com",
         itemCopy: "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics.  Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, it's the dawn of apparel that truly works for you.",
         itemImg: "./img-1.jpg",
     },
